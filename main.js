ed3df45fbd354a5dab44a0876f5f50451f583dbe
funposter <artemijaksenov891@gmail.com>
2023-02-07 10:08:21 +0500

    finish game

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const board = document.querySelector('#board')
+
 const squareNumber = 500
+
 const colors = ['#F39F18', '#F75E25', '#FAD201', '#256D7B', '#F44611', '#606E8C', '#2271B3', '#F54021']
 
 for(let i = 0; i < squareNumber; i++){
     const square = document.createElement('div')
     square.classList.add('square')
-    
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+
+    square.addEventListener('mouseleave', () => {
+        removeColor(square)
+    })
+
     board.append(square)
 }
 
@@ -16,8 +25,12 @@ function randomColor(){
 }
 
 function setColor(element){
-
+    const color = randomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
-function removeColor(element){
 
+function removeColor(element){
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
 }
