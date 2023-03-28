f0d969a1deea52f513fde77d189be9b3608940bc
San4eZzZgg <terekhovmega@gmail.com>
2023-03-28 10:20:58 +0500

    finish

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
-const figure = document.querySelector('.second-figure')
-const mainFigure = document.querySelector('.main-figure')
+const body = document.querySelector('body')
+const board = document.querySelector('#board')
 
-function getRandomNumber(min, max) {
-    return Math.floor(min + Math.random() * (max + 1 - min))
-}
+const colors = ['#343E40', '#6C4675', '#412227', '#8A6642', '#CB2821',
+    '#B44C43', '#9D9101', '#7E7B52', '#C2B078', '#474A51']
 
-function createRandomFigure(){
-    const figure = document.createElement('div')
-    const size = getRandomNumber(30, 60)
-    const {width, height} = mainFigure.getBoundingClientRect()
-    const x = getRandomNumber (0, width-size)
-    const y = getRandomNumber (0, height-size)
-
-    figure.classList.add('second-figure')
-    figure.style.width = `${size}px`
-    figure.style.height = `${size}px`
-    figure.style.top = `${y}px`
-    figure.style.left = `${x}px`
-    figure.style.background = '#ffd4b3'
-    figure.style.margin = `${getRandomNumber(10, 100)}px`
-
-    mainFigure.append(figure)
-}
-
-document.addEventListener('keydown',(e)=>{
-    if (e.keyCode === 32){
-        createRandomFigure()
+body.addEventListener('keydown', (e) => {
+    if (e.code === 'Space') {
+        addFigure()
     }
 })
+const figures = ['circle', 'square','oval'] 
+function addFigure() {
+    const newFirugre = document.createElement('div')
+    // Разный цвет
+    const randomColor = colors[rnd(0, colors.length - 1)]
+    newFirugre.style.background = randomColor
+    const type = figures[rnd(0, figures.length)]
+
+    addStyles(newFirugre, type)
+    board.append(newFirugre)
+}
+
+function addStyles(figure, type) {
+    figure.classList.add(type)
+    switch (type) {
+        case 'circle': {
+            const size = rnd(10, 70)
+            figure.style.width = size + 'px'
+            figure.style.height = size + 'px'
+            const { width, height } = board.getBoundingClientRect()
+            figure.style.top = rnd(0, width - size) + 'px'
+            figure.style.left = rnd(0, height - size) + 'px'
+            break
+        }
+        case 'square': {
+            const size = rnd(10, 70)
+            figure.style.width = size + 'px'
+            figure.style.height = size + 'px'
+            const { width, height } = board.getBoundingClientRect()
+            figure.style.top = rnd(0, width - size) + 'px'
+            figure.style.left = rnd(0, height - size) + 'px'
+            break
+        }
+        case 'oval': {
+            const size = rnd(30,100)
+            figure.style.width = size*2 + 'px'
+            figure.style.height = size + 'px'
+            figure.style.borderRadius = `${size}px / ${size/2}px`
+            const { width, height } = board.getBoundingClientRect()
+            figure.style.top = rnd(0, width - size) + 'px'
+            figure.style.left = rnd(0, height - size*2) + 'px'
+            break
+        }
+    }
+}
+
+
+
+
+function rnd(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
