3b634abe1f29b706c2e4872253b8d3843496f265
San4eZzZgg <terekhovmega@gmail.com>
2023-04-18 10:23:23 +0500

    init

--- square.js
+++ square.js
@@ -3,6 +3,7 @@ class Square {
         this.size = size
         this.color = color
         this.board = board
+        this.position = position
     }
     init() {
         const square = document.createElement('div')
@@ -10,9 +11,14 @@ class Square {
         square.style.width = `${this.size}px`
         square.style.height = `${this.size}px`
         square.style.background = this.color
-
-        square.style.right = '0'
-        square.style.top = '0'
+        const boardWidth = parseInt(window.getComputedStyle(thid.board).width)
+        if(this.position == 1) {
+            square.style.right = '0'
+            square.style.top = boardWidth - this.size + 'px'
+        } else if (this.position == 2) { 
+            square.style.top = boardWidth - this.size + 'px'
+            square.style.right = '0'
+        }
 
         this.board.append(square)
     }
